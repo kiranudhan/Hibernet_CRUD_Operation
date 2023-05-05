b870aecb3434f38fcc65be0cad2d5dc8606a8522
kiranudhan <udhankiran62@gmail.com>
2023-05-05 13:43:11 +0530

    record inserted

--- src/main/java/com/crud_operation/App.java
+++ src/main/java/com/crud_operation/App.java
@@ -1,13 +1,33 @@
 package com.crud_operation;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        
+        SessionFactory sessionFactory=configuration.buildSessionFactory();
+        
+        Session session=sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        
+        //to insert record we will create object
+        Student student = new Student();
+        student.setName("kiran");
+        student.setCity("jalna");
+        student.setMobile("9872625832");
+        
+        session.save(student);
+        transaction.commit();
+        
+        session.close();
+        
+        System.out.println("record inserted successfully");
     }
 }
